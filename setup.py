c04ff7b8e91388c451ff7f6392f237e02d6cb50f
Tyler Yep <tyep@cs.stanford.edu>
2020-10-27 15:12:33 -0700

    convert setup.py to metadata

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md") as f:
-    LONG_DESCRIPTION = f.read()
-
-setuptools.setup(
-    name="probs",
-    version="0.0.4",
-    author="Tyler Yep",
-    author_email="tyep@cs.stanford.edu",
-    description="Probability library for Python",
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
-    url="https://github.com/tyleryep/probs",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.7",
-)
+setup()
